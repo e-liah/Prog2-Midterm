95d52b8a8d8aa7c87b16167bb2c25462795bf19d
e-liah <111042233+e-liah@users.noreply.github.com>
2023-02-11 21:55:50 +0800

    Update midterm-str.cpp

--- midterm-str.cpp
+++ midterm-str.cpp
@@ -4,7 +4,7 @@ if the input is in lowercase, convert into uppercase
 if the input is in uppercase, convert into lowercase
 ask the user if they want to try it again
 if yes, repeat -- save the previous input in an array
-if no, display the previous inputs and then terminate
+if no, terminate
 */
 
 #include <iostream>
@@ -17,56 +17,45 @@ using namespace std;
 
 int main() {
 
-  string name, hold;
+  string name;
   char choice;
-  int i, j;
+  int i;
 
   start:
-  cout << "\nEnter a name: ";
-  getline(cin, name);
-
-  //hold = name;
+  cout << "\n";
+  cout << "Hi vlog, welcome to my everyone!\n\n";
+  cout << "Enter a name: ";
+  getline (cin, name);
 
   for (int i = 0; name[i] != '\0'; i++) {
-    if (islower(name[i])) { //converts lowercase letters into uppercase
-      hold[i] = toupper(name[i]);
+    if (name[i] >= 97 && name[i] <= 122) { //converts lowercase letters into uppercase
+      name[i] = toupper(name[i]);
     }
 
-    else if (isupper(name[i])) { //converts uppercase letters into lowercase
-      hold[i] = tolower(name[i]);
-    }
-
-    if (hold[i] == '\0') {
-      name[i] += hold[i];
+    else if (name[i] >= 65 && name[i] <= 90) { //converts uppercase letters into lowercase
+      name[i] = tolower(name[i]);
     }
   }
+ 
+  cout << "\n";
+  cout << "Its showtime!!!\n\n";
+  Sleep(1000); //slight delay for effects
+  cout << name << "\n\n";
 
-  for (int i = 0; name[i] != '\0'; i++) {
-      cout << name[i];
-    }
+  Sleep(3000);
 
   choose:
   cout << "\nWould you like to try again? [Y/N] ";
   cin >> choice;
   cin.ignore();
 
-  if (choice == 'Y' || choice == 'y') {  //need to save the input 
+  if (choice == 'Y' || choice == 'y') {
+    //need to save the input first before clearing the screen
+    system("cls");
     goto start;
   }
 
   else if (choice == 'N' || choice == 'n') {
-    //for (int i = 0; memo[i] != '\0'; i++) {
-      /*for (int j = 0; name[j] != '\0'; j++, i++){
-        memo[i] = name[j];
-      }*/
-
-     for (int i = 0; i < hold[i]; i++) {
-      name[i] = hold[i];
-      name += hold[i];
-    }
-
-    cout << "\n" << name << " ";
-
     cout << "\n\nClosing the program...";
     Sleep(3000);
   }
